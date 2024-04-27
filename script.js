381098b7d88c145b4e559a2a83e8fcbcca427a0b
Kawsar <mdk115599@gmail.com>
2024-04-27 20:55:07 +0600

    add localStorage

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-const input = document.getElementById('input');
-const btn = document.getElementById('btn');
-const list = document.getElementById('list');
+const input = document.getElementById("input");
+const btn = document.getElementById("btn");
+const list = document.getElementById("list");
 
 btn.onclick = () => {
-  if (input.value === '') {
-    alert('You must add something');
+  if (input.value === "") {
+    alert("You must add something");
   } else {
-    let li = document.createElement('li');
+    let li = document.createElement("li");
     li.innerHTML = input.value;
     list.appendChild(li);
   }
-  input.value = '';
+  input.value = "";
+  saveData();
 };
 
-list.addEventListener('click', function (e) {
+list.addEventListener("click", function (e) {
   e.target.remove();
+  saveData();
 });
+
+function saveData() {
+  localStorage.setItem("data", list.innerHTML);
+}
+
+function showData() {
+  list.innerHTML = localStorage.getItem("data");
+}
+
+showData();
